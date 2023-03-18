5adc212da6da260827c63e0c30c8aa7389583ba8
Martin Roa Villescas <mroavi@gmail.com>
2023-03-18 20:11:50 +0100

    Config rose-pine colors

--- config.def.h
+++ config.def.h
@@ -3,9 +3,17 @@ static const char *user  = "mroavi";
 static const char *group = "mroavi";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#73cef4",   /* during input */
-	[FAILED] = "#f43753",   /* wrong password */
+
+  [INIT] =   "black",     /* after initialization */
+
+  //// tender
+  //[INPUT] =  "#73cef4",   /* during input */
+  //[FAILED] = "#f43753",   /* wrong password */
+
+  // rose-pine
+  [INPUT] =  "#26233a",   /* during input */
+  [FAILED] = "#eb6f92",   /* wrong password */
+
 };
 
 /* treat a cleared input like a wrong password (color) */
