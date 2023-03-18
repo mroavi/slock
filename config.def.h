98c50c23348bd898e2fa8f1fce74caa46d5eea8c
Martin Roa Villescas <mroavi@gmail.com>
2023-03-18 19:44:08 +0100

    Config tender colors

--- config.def.h
+++ config.def.h
@@ -1,11 +1,11 @@
 /* user and group to drop privileges to */
-static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *user  = "mroavi";
+static const char *group = "mroavi";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[INPUT] =  "#73cef4",   /* during input */
+	[FAILED] = "#f43753",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
